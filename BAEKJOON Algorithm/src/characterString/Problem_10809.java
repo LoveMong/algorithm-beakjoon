8c7bf6f9b253f3a729cb46af1810b3f4d27166dc
LeeMong <izpzgi2019@gmail.com>
2022-05-07 00:18:23 +0900

    10809번 알파벳 찾기

--- BAEKJOON Algorithm/src/characterString/Problem_10809.java
+++ BAEKJOON Algorithm/src/characterString/Problem_10809.java
@@ -2,32 +2,31 @@ package characterString;
 
 import java.util.Scanner;
 
+/*
+알파벳 소문자로만 이루어진 단어 S가 주어진다. 각각의 알파벳에 대해서,
+단어에 포함되어 있는 경우에는 처음 등장하는 위치를, 포함되어 있지 않은 경우에는 -1을 출력하는 프로그램을 작성하시오.
+ */
 public class Problem_10809 {
 	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		String word = sc.next();
+		sc.close();
 		char[] alphabet = {'a','b','c','d','e','f','g','h','i','j',
 						   'k','l','m','n','o','p','q','r','s','t',
-						   'u','v','w','x','y','z'};
+						   'u','v','w','x','y','z'};		
 		
-//		char[] cWord = new char[word.length()];
-//		for(int i = 0; i < word.length(); i++) {
-//			
-//			cWord[i] = word.charAt(i);
-//			
-//		}
-		
-		int confirm = 0;
 		
 		for(int i = 0; i < alphabet.length; i++) {
+		
+			int confirm = 0;
 			
 			for(int j = 0; j < word.length(); j++) {
 				
 				if(alphabet[i] == word.charAt(j)) {
 					
-					System.out.print(j);
+					System.out.print(j + " ");
 					
 					confirm = 1;
 					
@@ -37,9 +36,11 @@ public class Problem_10809 {
 				
 			}
 			
-				if(confirm == 0)
+			if (confirm == 0 ) {
 				
-				System.out.print("-1 ");					
+				System.out.print("-1 ");
+		
+			}										
 			
 		}
 		
