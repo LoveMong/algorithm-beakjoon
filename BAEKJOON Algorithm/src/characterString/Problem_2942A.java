8d77f89d0933f18a1a98b0b334ef81a765bb8a23
LeeMong <izpzgi2019@gmail.com>
2022-06-23 22:18:48 +0900

    2941A

---
+++ BAEKJOON Algorithm/src/characterString/Problem_2942A.java
@@ -0,0 +1,121 @@
+package characterString;
+
+import java.util.Scanner;
+
+// 크로아티아 알파벳
+public class Problem_2942A {
+	
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		
+		int count = 0;
+		
+		for (int i = 0; i < str.length(); i++) {
+			
+			if(str.charAt(i) == 'c') {
+				
+				if(i < str.length() - 1) {
+					
+					if(str.charAt(i + 1) == '=') {
+						
+						i++;
+						
+					} else if(str.charAt(i + 1) == '-') {
+						
+						i++;
+					}
+					
+				}
+				
+			} else if (str.charAt(i) == 'd') {
+				
+				if(i < str.length() - 1) {
+					
+					if(str.charAt(i + 1) == '-') {
+						
+						i++;
+						
+					} else if(str.charAt(i + 1) == 'z') {
+						
+						if(i < str.length() - 2) {
+							
+							if(str.charAt(i + 2) == '=') {
+								
+								i += 2;
+							}
+							
+						}					
+						
+					}
+					
+				}		
+			
+			} else if (str.charAt(i) == 'l') {
+				
+				if(i < str.length() - 1) {
+					
+					if(str.charAt(i + 1) == 'j' ) {
+						
+						i++;
+						
+					}
+					
+				}
+				
+			} else if (str.charAt(i) == 'n') {
+				
+				if(i < str.length() -1) {
+					
+					if(str.charAt(i + 1) == 'j') {
+						
+						i++;
+						
+					}
+					
+				}
+				
+			} else if (str.charAt(i) == 's') {
+				
+				if(i < str.length() -1) {
+					
+					if(str.charAt(i + 1) == '=') {
+						
+						i++;
+						
+					}
+					
+				}
+				
+			} else if (str.charAt(i) == 'z') {
+				
+				if(i < str.length() -1) {
+					
+					if(str.charAt(i + 1) == '=') {
+						
+						i++;
+						
+					}
+					
+				}
+				
+			}
+		
+		
+			count++;
+			
+		}	
+		
+		System.out.println(count);
+		
+	}
+		
+}
+		
+
+	
+	
+
+
