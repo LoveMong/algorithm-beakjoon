bd1581e3b63c07ac62f733e32f0962a2fc271124
LeeMong <izpzgi2019@gmail.com>
2022-08-13 09:54:51 +0900

    Problem_2438

---
+++ BAEKJOON Algorithm/src/loop/Problem_2438R.java
@@ -0,0 +1,37 @@
+package loop;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// 별 찍기
+public class Problem_2438R {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int testCaseNum = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= testCaseNum; i++) {
+				
+			for (int j = 1; j <= i; j++) {
+				
+				bw.write("*");
+				
+			}
+			
+			bw.newLine();
+			
+		}
+		
+		bw.flush();
+		bw.close();
+		
+		br.close();
+		
+	}
+}
