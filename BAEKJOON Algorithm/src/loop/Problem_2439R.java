ab68063d6392df7969f5c096d979dd3dcee600cf
LeeMong <izpzgi2019@gmail.com>
2022-08-14 23:14:31 +0900

    Problem_2438R

--- BAEKJOON Algorithm/src/loop/Problem_2439R.java
+++ BAEKJOON Algorithm/src/loop/Problem_2439R.java
@@ -6,25 +6,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-// 별 찍기
-public class Problem_2438R {
+// 별 찍기 2
+public class Problem_2439R {
 
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int testCaseNum = Integer.parseInt(br.readLine());
 		
+		
 		for (int i = 1; i <= testCaseNum; i++) {
+			
+			for (int j = 1; j <= testCaseNum - i; j++) {
 				
-			for (int j = 1; j <= i; j++) { // 첫 번째 for문 i만큼 별을 출력
+				bw.write(" ");
+				
+			}
+			
+			for (int k = 1; k <= i; k++) {
 				
 				bw.write("*");
 				
 			}
 			
-			bw.newLine(); // 두 번째 for문 완료 이후 줄바꿈
+			bw.newLine();
 			
 		}
 		
@@ -32,6 +39,7 @@ public class Problem_2438R {
 		bw.close();
 		
 		br.close();
+		
 		
 	}
 }
