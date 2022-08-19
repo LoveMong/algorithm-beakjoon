f7922d7c5d63abd8e5c8c130cef379024b273a0a
LeeMong <izpzgi2019@gmail.com>
2022-08-19 16:18:34 +0900

    Problem_1110R

--- BAEKJOON Algorithm/src/loop/Problem_1110R.java
+++ BAEKJOON Algorithm/src/loop/Problem_1110R.java
@@ -11,17 +11,20 @@ public class Problem_1110R {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = Integer.parseInt(br.readLine());
-		int result = 0;
-		int A = N;
+		int N = Integer.parseInt(br.readLine()); // 테스트 정수 입력 받음
+		int result = 0; // 입력받은 정수로 몇 번 만에 돌아오는지 체크하는 기준
+		int A = N; // 새로운 수를 저장하기 위한 변수
 		
 		br.close();
 		
 		
 		do {
+			
 			result++;
-			A = ((A % 10) * 10) + (((A / 10) + (A % 10)) % 10); 
-		} while(N != A);
+			
+			A = ((A % 10) * 10) + (((A / 10) + (A % 10)) % 10);
+			
+		} while(N != A); // 입력받은 정수와 새로운 수가 같아질 때까지 반복문 실행
 			
 		System.out.println(result);
 		
