23763c4c1bd1c0d5d9da14025043b3f0125f94ee
LeeMong <izpzgi2019@gmail.com>
2022-08-22 10:50:18 +0900

    Problem_3052R

--- BAEKJOON Algorithm/src/array/Problem_3052R.java
+++ BAEKJOON Algorithm/src/array/Problem_3052R.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
 
+
 // 나머지
 public class Problem_3052R {
 	
@@ -13,10 +14,9 @@ public class Problem_3052R {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		
+		int[] arrNum = new int[10]; // 입력받은 10개의 정수를 각각 42로 나눈 후 나머지를 담을 변수
 		
-		int[] arrNum = new int[10];
-		
-		for (int i = 0; i < arrNum.length; i++) {
+		for (int i = 0; i < arrNum.length; i++) { // 배열 변수에 입력받은 순서대로 42로 나누고 나머지를 저장
 			
 			arrNum[i] = Integer.parseInt(br.readLine()) % 42;
 			
@@ -25,16 +25,16 @@ public class Problem_3052R {
 		
 		br.close();
 		
-		HashSet<String> hashset = new HashSet<>();
+		HashSet<String> hashset = new HashSet<>(); // HashSet 생성
 		
 		
-		for (int num : arrNum) {
+		for (int num : arrNum) { // HashSet 값 추가 기능을 이용 위에서 구한 나머지값들을 중복을 제거하며 추가 
 			
 			hashset.add(String.valueOf(num));
 			
 		}
 		
-		System.out.println(hashset.size());
+		System.out.println(hashset.size()); // HashSet에 저장된 값의 갯수를 출력
 		
 	}
 
